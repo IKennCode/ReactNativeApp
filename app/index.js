ec6dd44d35e7f5abfed4c41d0f54270b9e3f16e3
Kenneth Dimalibot <dimalibotkennethofficial@gmail.com>
2023-11-21 15:23:40 +0800

    new

---
+++ app/index.js
@@ -0,0 +1,11 @@
+import {View, Text} from 'react-native';
+
+const Home = () => {
+    return (
+        <View>
+            <Text>Home Based</Text>
+        </View>
+    )
+}
+
+export default Home;
